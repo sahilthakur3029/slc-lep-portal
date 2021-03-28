1d9dcd02627bc038ffe12505085c651253e079a8
Sahil Thakur <sahilthakur@berkeley.edu>
2021-03-28 00:46:43 -0700

    added algo file but route broken

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from routes.intakeform import intakeform
 from routes.timesheetform import timesheetform
+from routes.algorithm import algorithm
 from flask_cors import CORS
 import psycopg2
 
@@ -8,6 +9,7 @@ import psycopg2
 app = Flask(__name__)
 app.register_blueprint(intakeform)
 app.register_blueprint(timesheetform)
+app.register_blueprint(algorithm)
 CORS(app)
 
 # Connect to your postgres DB
---
+++ server/routes/algorithm.py
@@ -0,0 +1,30 @@
+from flask import Flask, Blueprint, jsonify, request
+from flask_cors import CORS
+import psycopg2
+import pandas as pd
+
+intakeform = Blueprint('algorithm', __name__, template_folder='templates')
+CORS(intakeform)
+
+# Connect to your postgres DB
+conn = psycopg2.connect("dbname=slcapplication user=postgres")
+
+def postgresql_to_dataframe(conn, select_query, column_names):
+    """
+    Tranform a SELECT query into a pandas dataframe
+    """
+    cursor = conn.cursor()
+    try:
+        cursor.execute(select_query)
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Error: %s" % error)
+        cursor.close()
+        return 1
+    
+    # Naturally we get a list of tupples
+    tupples = cursor.fetchall()
+    cursor.close()
+    
+    # We just need to turn it into a pandas dataframe
+    df = pd.DataFrame(tupples, columns=column_names)
+    return df

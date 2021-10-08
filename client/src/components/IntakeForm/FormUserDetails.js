51e343be3481e6f65a511976abd5b747a7831910
Sahil Thakur <sahilthakur@berkeley.edu>
2021-10-08 13:50:25 -0700

    aligning first intake form page

--- client/src/components/IntakeForm/FormUserDetails.js
+++ client/src/components/IntakeForm/FormUserDetails.js
@@ -31,6 +31,9 @@ const useStyles = (theme) => ({
     textAlign: "center",
     fontSize: 50,
   },
+  elements: {
+    textAlign: "center",
+  },
   selectEmpty: {
     marginTop: theme.spacing(2),
   },
@@ -134,175 +137,181 @@ class FormUserDetails extends Component {
             LEP {this.state.semester} Intake Form
           </h1>
           <br />
-          <h2 className={classes.formControl}>Basic Information</h2>
-          <TextField
-            placeholder="Enter Your First Name"
-            label="First Name"
-            onChange={handleChange("firstName")}
-            defaultValue={values.firstName}
-            margin="normal"
-            required
-            className={classes.formControl}
-          />
-          <TextField
-            placeholder="Enter Your Last Name"
-            label="Last Name"
-            onChange={handleChange("lastName")}
-            defaultValue={values.lastName}
-            margin="normal"
-            required
-            className={classes.formControl}
-          />
-          <br />
-          <TextField
-            placeholder="Enter Your Email"
-            label="Email"
-            onChange={handleChange("email")}
-            defaultValue={values.email}
-            margin="normal"
-            required
-            className={classes.formControl}
-          />
-          <br />
-          <FormControl className={classes.formControl} required>
-            <InputLabel id="academic-title-label">Academic Title</InputLabel>
-            <Select
-              labelId="academic-title-label"
-              id="academic-title"
-              defaultValue={values.academicTitle}
-              onChange={handleChange("academicTitle")}
-            >
-              <MenuItem value={"Undergraduate"}>Undergraduate</MenuItem>
-              <MenuItem value={"Graduate"}>Graduate</MenuItem>
-              <MenuItem value={"Staff"}>Staff</MenuItem>
-              <MenuItem value={"Scholar"}>Scholar</MenuItem>
-              <MenuItem value={"Alumnus"}>Alumnus</MenuItem>
-            </Select>
-          </FormControl>
-          <FormControl className={classes.formControl} required>
-            <InputLabel id="residency-label">Residency</InputLabel>
-            <Select
-              labelId="residency-label"
-              id="residency"
-              defaultValue={values.residency}
-              onChange={handleChange("residency")}
-            >
-              <MenuItem value={"Domestic US"}>Domestic US</MenuItem>
-              <MenuItem value={"International"}>International</MenuItem>
-            </Select>
-          </FormControl>
-          <br />
-          <TextField
-            placeholder="Enter Your Major(s)"
-            label="Major(s) - If multiple, seperate with commas"
-            onChange={handleChange("major")}
-            defaultValue={values.major}
-            margin="normal"
-            className={classes.formControl}
-          />
-          <br />
-          <Tooltip
-            title="We use the following information for the purposes of helping our staff use the most 
-          respectful language when addressing you and giving you the option, as seen later in this form, 
-          of preferring a partner with a particular gender."
-            placement="top"
-          >
-            <FormControl className={classes.formControl}>
-              <InputLabel id="gender-label">
-                Gender <HelpIcon className={classes.questionMark}></HelpIcon>
-              </InputLabel>
+          <div className={classes.elements}>
+            <h2 className={classes.formControl}>Basic Information</h2>
+            <TextField
+              placeholder="Enter Your First Name"
+              label="First Name"
+              onChange={handleChange("firstName")}
+              defaultValue={values.firstName}
+              margin="normal"
+              required
+              className={classes.formControl}
+            />
+            <TextField
+              placeholder="Enter Your Last Name"
+              label="Last Name"
+              onChange={handleChange("lastName")}
+              defaultValue={values.lastName}
+              margin="normal"
+              required
+              className={classes.formControl}
+            />
+            <TextField
+              placeholder="Enter Your Email"
+              label="Email"
+              onChange={handleChange("email")}
+              defaultValue={values.email}
+              margin="normal"
+              required
+              className={classes.formControl}
+            />
+            <br />
+            <FormControl className={classes.formControl} required>
+              <InputLabel id="academic-title-label">Academic Title</InputLabel>
               <Select
-                labelId="gender-label"
-                id="gender"
-                defaultValue={values.gender}
-                onChange={handleChange("gender")}
+                labelId="academic-title-label"
+                id="academic-title"
+                defaultValue={values.academicTitle}
+                onChange={handleChange("academicTitle")}
               >
-                <MenuItem value={"Male"}>Male</MenuItem>
-                <MenuItem value={"Female"}>Female</MenuItem>
-                <MenuItem value={"TransMale"}>TransMale</MenuItem>
-                <MenuItem value={"TransFemale"}>TransFemale</MenuItem>
-                <MenuItem value={"Genderqueer"}>Genderqueer</MenuItem>
-                <MenuItem value={"Prefer Not To State"}>
-                  Prefer Not To State
-                </MenuItem>
-                <MenuItem value={"Custom"}>Custom</MenuItem>
+                <MenuItem value={"Undergraduate"}>Undergraduate</MenuItem>
+                <MenuItem value={"Graduate"}>Graduate</MenuItem>
+                <MenuItem value={"Staff"}>Staff</MenuItem>
+                <MenuItem value={"Scholar"}>Scholar</MenuItem>
+                <MenuItem value={"Alumnus"}>Alumnus</MenuItem>
               </Select>
             </FormControl>
-          </Tooltip>
-          <TextField
-            placeholder="Gender Custom"
-            label="If Custom, please specify"
-            onChange={handleChange("genderCustom")}
-            defaultValue={values.genderCustom}
-            margin="normal"
-            className={classes.formControl}
-          />
-          <br />
-          <FormControl className={classes.formControl}>
-            <InputLabel id="days-label">Select All Available Days</InputLabel>
-            <Select
-              labelId="dow-label"
-              id="dow"
-              multiple
-              value={values.availability}
-              onChange={handleChange("availability")}
-              input={<Input />}
-              MenuProps={MenuProps}
+            <FormControl className={classes.formControl} required>
+              <InputLabel id="residency-label">Residency</InputLabel>
+              <Select
+                labelId="residency-label"
+                id="residency"
+                defaultValue={values.residency}
+                onChange={handleChange("residency")}
+              >
+                <MenuItem value={"Domestic US"}>Domestic US</MenuItem>
+                <MenuItem value={"International"}>International</MenuItem>
+              </Select>
+            </FormControl>
+            <TextField
+              placeholder="Enter Your Major(s)"
+              label="Major(s) - If multiple, seperate with commas"
+              onChange={handleChange("major")}
+              defaultValue={values.major}
+              margin="normal"
+              className={classes.formControl}
+            />
+            <br />
+            <Tooltip
+              title="We use the following information for the purposes of helping our staff use the most 
+          respectful language when addressing you and giving you the option, as seen later in this form, 
+          of preferring a partner with a particular gender."
+              placement="top"
             >
-              {allDays.map((name) => (
-                <MenuItem key={name} value={name}>
-                  {name}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <br />
-          <TextField
-            label="What do you hope to gain from the SLC Language Exchange Program?"
-            onChange={handleChange("hopeToGain")}
-            defaultValue={values.hopeToGain}
-            margin="normal"
-            required
-            fullWidth
-            className={classes.formControl}
-          />
-          <br />
-          <TextField
-            label="How do you plan to maintain your motivation to meet with your partner(s) weekly?"
-            onChange={handleChange("planToMeet")}
-            defaultValue={values.planToMeet}
-            margin="normal"
-            required
-            fullWidth
-            className={classes.formControl}
-          />
-          <br />
-          <br />
-          <h2 className={classes.formControl}>
-            Please Enter The Key You Receieved At Orientation Below (You Will
-            Not Be Able To Continue Without The Correct Key):
-          </h2>
-          <TextField
-            placeholder="Orienataion Key"
-            label="Enter orientation key here"
-            onChange={handleChange("orientationKey")}
-            defaultValue={values.orientationKey}
-            margin="normal"
-            required
-            fullWidth
-            className={classes.formControl}
-          />
-          <br />
-          <br />
-          <ColorButton
-            variant="contained"
-            color="primary" // Looks like this could be secondary also without making a difference
-            className={classes.margin}
-            onClick={this.continue}
-          >
-            Next
-          </ColorButton>
-          <br />
+              <FormControl className={classes.formControl}>
+                <InputLabel id="gender-label">
+                  Gender <HelpIcon className={classes.questionMark}></HelpIcon>
+                </InputLabel>
+                <Select
+                  labelId="gender-label"
+                  id="gender"
+                  defaultValue={values.gender}
+                  onChange={handleChange("gender")}
+                >
+                  <MenuItem value={"Male"}>Male</MenuItem>
+                  <MenuItem value={"Female"}>Female</MenuItem>
+                  <MenuItem value={"TransMale"}>TransMale</MenuItem>
+                  <MenuItem value={"TransFemale"}>TransFemale</MenuItem>
+                  <MenuItem value={"Genderqueer"}>Genderqueer</MenuItem>
+                  <MenuItem value={"Prefer Not To State"}>
+                    Prefer Not To State
+                  </MenuItem>
+                  <MenuItem value={"Custom"}>Custom</MenuItem>
+                </Select>
+              </FormControl>
+            </Tooltip>
+            <TextField
+              placeholder="Gender Custom"
+              label="If Custom, please specify"
+              onChange={handleChange("genderCustom")}
+              defaultValue={values.genderCustom}
+              margin="normal"
+              className={classes.formControl}
+            />
+            <FormControl className={classes.formControl}>
+              <InputLabel id="days-label">Select All Available Days</InputLabel>
+              <Select
+                labelId="dow-label"
+                id="dow"
+                multiple
+                value={values.availability}
+                onChange={handleChange("availability")}
+                input={<Input />}
+                MenuProps={MenuProps}
+              >
+                {allDays.map((name) => (
+                  <MenuItem key={name} value={name}>
+                    {name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <br />
+            <br />
+            <TextField
+              style={{
+                margin: 0,
+                width: "77%",
+              }}
+              label="What do you hope to gain from the SLC Language Exchange Program?"
+              onChange={handleChange("hopeToGain")}
+              defaultValue={values.hopeToGain}
+              margin="normal"
+              required
+              className={classes.formControl}
+            />
+            <br />
+            <br />
+            <TextField
+              style={{
+                margin: 0,
+                width: "77%",
+              }}
+              label="How do you plan to maintain your motivation to meet with your partner(s) weekly?"
+              onChange={handleChange("planToMeet")}
+              defaultValue={values.planToMeet}
+              margin="normal"
+              required
+              className={classes.formControl}
+            />
+            <br />
+            <br />
+            <h2 className={classes.formControl}>
+              Please Enter The Key You Receieved At Orientation Below (You Will
+              Not Be Able To Continue Without The Correct Key):
+            </h2>
+            <TextField
+              placeholder="Orienataion Key"
+              label="Enter orientation key here"
+              onChange={handleChange("orientationKey")}
+              defaultValue={values.orientationKey}
+              margin="normal"
+              required
+              className={classes.formControl}
+            />
+            <br />
+            <br />
+            <ColorButton
+              variant="contained"
+              color="primary" // Looks like this could be secondary also without making a difference
+              className={classes.margin}
+              onClick={this.continue}
+            >
+              Next
+            </ColorButton>
+            <br />
+          </div>
         </>
       </MuiThemeProvider>
     );
